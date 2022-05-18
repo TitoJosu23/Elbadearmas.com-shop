8d9160262fb7f22b914545ffe46732f1175578e6
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-18 09:23:09 +0000

    resolving problems in footer

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -8,8 +8,14 @@ export const Home = () => {
 
 	return (
 		<div className="text-center mt-5">
-			<div>
-				<h3>Este es el comienzo de algo grande</h3>
+			<div className="container-home">
+				<div className="img-part">
+					<img src="https://i.redd.it/52f61nfzmwl51.jpg" className="img-home"></img>
+				</div>
+				<div className="welcome-home">
+					<h2>¡Bienvenidos!</h2>
+					<p>Hola, Soy Elba y me encantan que estes aquí espero que disfrutes de mi contenido y todo lo que tengo para ofrecerte  ¡Besos!  </p>
+				</div>
 			</div>
 		</div>
 	);
