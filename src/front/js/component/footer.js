b9a915749b989bc5d8126af3964440950585b63a
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-19 08:30:04 +0000

    footer 1

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,7 +4,7 @@ import "../../styles/footer.css";
 export const Footer = () => (
 	<footer className="text-center footer">
 		<div className="container-footer">
-			<a href="https://www.google.com" target="_blank">Política de privacidad</a>
+			<a href="https://www.google.com" target="_blank">Política de privacidad</a> 
 			<a href="https://www.google.com" target="_blank">Términos y condiciones</a>
 			<a href="https://www.google.com" target="_blank">Return Policy</a>
 			<a href="https://www.google.com" target="_blank">Shipping Policy</a>
