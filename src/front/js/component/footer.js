6a04695548e850f619885192b50f710ae6396623
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-20 05:25:47 +0000

    changing the footers a href=

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -4,14 +4,10 @@ import "../../styles/footer.css";
 export const Footer = () => (
 	<footer className="text-center footer">
 		<div className="container-footer">
-			<a href="https://www.google.com" target="_blank">Política de privacidad</a> 
-			<a href="https://www.google.com" target="_blank">Términos y condiciones</a> 
-			<a href="https://www.google.com" target="_blank">Return Policy</a> 
-			<a href="https://www.google.com" target="_blank">Shipping Policy</a> 
+			<a href="PrivacyPolicy" target="_blank">Política de privacidad</a> 
+			<a href="TermsConditions" target="_blank">Términos y condiciones</a> 
+			<a href="ReturnPolicy" target="_blank">Return Policy</a> 
+			<a href="ShippingPolicy" target="_blank">Shipping Policy</a> 
 		</div> 
 </footer> 
 );
-
-
-// position absolute 
-// bottom 0 
