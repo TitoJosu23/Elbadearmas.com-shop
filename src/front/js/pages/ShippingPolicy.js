288e749784bf317473239bdefd67624fbad971da
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-20 05:55:17 +0000

    finishing the Shipping Policy

--- src/front/js/pages/ShippingPolicy.js
+++ src/front/js/pages/ShippingPolicy.js
@@ -1,20 +1,22 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Link } from "react-router-dom";
-import { Context } from "../store/appContext";
+import React from "react";
+import "../../styles/ShippingPolicy.css";
 
 export const ShippingPolicy = () => {
 	return (
 		<>
-        <div class="SP-1">
-		    <h2>ShippingPolicy</h2>
-        </div>
-        <div class="SP-2">
-            <h4>SHIPPING</h4>
-        </div>
-        <div className="SP-3">
-            <p>U.S. orders over $150 are FREE. </p>
-            <p>Orders are processed and shipped in the order they are received. Shipping charges are nonrefundable.</p>
-            <p>Orders under $150 is pay for the client. </p>
+        <div className="container-sp">
+            <div class="sp-1">
+		        <h2>ShippingPolicy</h2>
+            </div>
+            <div className="to-line"></div>
+            <div class="sp-2">
+                <h4>SHIPPING</h4>
+            </div>
+            <div className="sp-3">
+                <p className="DeleteM-sp">U.S. orders over $150 are FREE. </p>
+                <p className="DeleteM-sp">Orders are processed and shipped in the order they are received. Shipping charges are nonrefundable.</p>
+                <p className="DeleteM-sp">Orders under $150 is pay for the client. </p>
+            </div>
         </div>
 		</>
 	);
