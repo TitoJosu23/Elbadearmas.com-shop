b37bd693e559abdac5d2eb163748fc8fe9e64827
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-20 05:16:00 +0000

    writing into the files of PP TC RP SP

--- src/front/js/pages/PrivacyPolicy.js
+++ src/front/js/pages/PrivacyPolicy.js
@@ -2,11 +2,10 @@ import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Podcast = () => {
+export const PrivacyPolicy = () => {
 	return (
 		<>
-		<h2>Hola Mundo</h2>
-		<h2>Podcast</h2>
+		<h2>PrivacyPolicy</h2>
 		</>
 	);
 };
