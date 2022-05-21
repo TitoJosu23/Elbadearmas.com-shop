34a6bd24a95dcaaceb1ace15441c82e68e3fe1f4
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-21 07:11:09 +0000

    adding the RP information

--- src/front/js/pages/ReturnPolicy.js
+++ src/front/js/pages/ReturnPolicy.js
@@ -3,9 +3,25 @@ import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const ReturnPolicy = () => {
-	return (
-		<>
-		<h2>ReturnPolicy</h2>
-		</>
-	);
+  return (
+    <>
+      <div class="container-rp">
+        <div>
+          <h2>ReturnPolicy</h2>
+        </div>
+        <div>
+          <h3>RETURN POLICY</h3>
+        </div>
+        <div>
+          <p>
+            We will accept returns for a full refund within 15 days of the date
+            your order was delivered. Returned items must be in their original
+            condition, unwashed, unworn and include tags. Exchanges: We are not
+            currently accepting exchanges. If your return is accepted, we will
+            issue a full refund to the original source of payment.{" "}
+          </p>
+        </div>
+      </div>
+    </>
+  );
 };
