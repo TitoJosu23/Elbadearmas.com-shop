06725d626c88660fc88466b91c8b9c983c0f291f
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-22 23:37:08 +0000

    resolving problems into the navbar

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,24 +1,13 @@
-import React, { useContext } from "react";
-import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
-import "../../styles/home.css";
+import React from "react";
+
+import "../../styles/CarouselTienda.css";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center">
-			<div className="container-home">
-				<div className="img-part">
-					<img src="https://i.redd.it/52f61nfzmwl51.jpg" className="img-home"></img>
-					<div></div>
-					<h2 className="tittle-in-img">Elba de Armas</h2>
-				</div>
-				<div className="welcome-home"> 
-					<h2 className="margin-top">¡Bienvenidos!</h2> 
-					<p>Hola, Soy Elba y me encantan que estes aquí espero que disfrutes de mi contenido y todo lo que tengo para ofrecerte  ¡Besos!  </p>
-				</div>
-			</div>
+		<div className="text-">
+			<h3>Esto es un h3</h3>
 		</div>
 	); 
 };
