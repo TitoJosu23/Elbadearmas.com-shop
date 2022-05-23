0377c1a4633773efd78cbecb261f119d2c08db52
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-23 00:48:47 +0000

    creating Carousel model

--- src/front/js/component/CarouselTienda.js
+++ src/front/js/component/CarouselTienda.js
@@ -4,8 +4,18 @@ import "../../styles/CarouselTienda.css";
 
 export const CarouselTienda = () => {
 	return (
-		<div className="text-center">
-			<h3>Hola QLQ</h3>
+		<div className="carousel-row">
+            <div>
+			    <h3 className="text-left">Tienda</h3>
+                    <div></div>
+            </div>
+            <div className="card">
+            <img className="card-img-top CT-img" src="https://via.placeholder.com/400x400" alt="Card image cap" />
+            <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
+    </div>
 		</div>
+        </div>
 	); 
 };
