892875a07e86541feea01a642d5dc997f3df8c65
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-23 00:13:33 +0000

    creating carousel file and solving problems

--- src/front/js/component/CarouselTienda.js
+++ src/front/js/component/CarouselTienda.js
@@ -1,34 +1,11 @@
 import React from "react";
-import Slider from "react-slick";
 
-export default function CarouselTienda() {
-  var settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1
-  };
-  return (
-    <Slider {...settings}>
-      <div>
-        <h3>1</h3>
-      </div>
-      <div>
-        <h3>2</h3>
-      </div>
-      <div>
-        <h3>3</h3>
-      </div>
-      <div>
-        <h3>4</h3>
-      </div>
-      <div>
-        <h3>5</h3>
-      </div>
-      <div>
-        <h3>6</h3>
-      </div>
-    </Slider>
-  );
-}
+import "../../styles/CarouselTienda.css";
+
+export const CarouselTienda = () => {
+	return (
+		<div className="text-center">
+			<h3>Hola QLQ</h3>
+		</div>
+	); 
+};
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { CarouselTienda } from "../component/CarouselTienda";
 
-import "../../styles/CarouselTienda.css";
+import "../../styles/home.css";
 
 export const Home = () => {
-	const { store, actions } = useContext(Context);
-
 	return (
-		<div className="text-">
-			<h3>Esto es un h3</h3>
+		<div className="text-center">
+			<div className="container-home">
+				<div className="img-part">
+					<img src="https://i.redd.it/52f61nfzmwl51.jpg" className="img-home"></img>
+					<div></div>
+					<h2 className="tittle-in-img">Elba de Armas</h2>
+				</div>
+					<CarouselTienda />
+				<div className="welcome-home"> 
+					<h2 className="margin-top">¡Bienvenidos!</h2> 
+					<p>Hola, Soy Elba y me encantan que estes aquí espero que disfrutes de mi contenido y todo lo que tengo para ofrecerte  ¡Besos!  </p>
+				</div>
+			</div>
 		</div>
 	); 
 };
