18c759f6ebd0ca6285d7416e714661216da19eb9
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-24 00:54:56 +0000

    updating the carousel

--- src/front/js/component/CarouselTienda.js
+++ src/front/js/component/CarouselTienda.js
@@ -3,19 +3,42 @@ import React from "react";
 import "../../styles/CarouselTienda.css";
 
 export const CarouselTienda = () => {
-	return (
-		<div className="carousel-row">
-            <div>
-			    <h3 className="text-left">Tienda</h3>
-                    <div></div>
-            </div>
-            <div className="card">
-            <img className="card-img-top CT-img" src="https://via.placeholder.com/400x400" alt="Card image cap" />
-            <div className="card-body">
+  return (
+    <div className="carousel-row">
+      <div>
+        <h3 className="text-left">Tienda</h3>
+        <div></div>
+      </div>
+      {/* este es la carta */}
+      <div className="card-group">
+        <div className="card card-carousel">
+          <img className="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap" />
+          <div className="card-body">
             <h5 className="card-title">Card title</h5>
-            <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p>
-    </div>
-		</div>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
         </div>
-	); 
+        <div className="card card-carousel">
+          <img className="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+        <div className="card card-carousel">
+          <img className="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap" />
+          <div className="card-body">
+            <h5 className="card-title">Card title</h5>
+            <p className="card-text">
+              <small className="text-muted">Last updated 3 mins ago</small>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -4,25 +4,26 @@ import { CarouselTienda } from "../component/CarouselTienda";
 import "../../styles/home.css";
 
 export const Home = () => {
-	return (
-		<div className="text-center">
-			<div className="container-home">
-				<div className="img-part">
-					<img src="https://i.redd.it/52f61nfzmwl51.jpg" className="img-home"></img>
-					<div></div>
-					<h2 className="tittle-in-img">Elba de Armas</h2>
-				</div>
-					<CarouselTienda />
-					<CarouselTienda />
-					<CarouselTienda />
-					<CarouselTienda />
-					<CarouselTienda />
-		
-				<div className="welcome-home"> 
-					<h2 className="margin-top">¡Bienvenidos!</h2> 
-					<p>Hola, Soy Elba y me encantan que estes aquí espero que disfrutes de mi contenido y todo lo que tengo para ofrecerte  ¡Besos!  </p>
-				</div>
-			</div>
-		</div>
-	); 
+  return (
+    <div className="text-center">
+      <div className="container-home">
+        <div className="img-part">
+          <img
+            src="https://i.redd.it/52f61nfzmwl51.jpg"
+            className="img-home"
+          ></img>
+          <div></div>
+          <h2 className="tittle-in-img">Elba de Armas</h2>
+        </div>
+        <CarouselTienda />
+        <div className="welcome-home">
+          <h2 className="margin-top">¡Bienvenidos!</h2>
+          <p>
+            Hola, Soy Elba y me encantan que estes aquí espero que disfrutes de
+            mi contenido y todo lo que tengo para ofrecerte ¡Besos!{" "}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
