8ccb2d75516ae475f6ee736a6524f871419f97e7
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-24 00:58:39 +0000

    adding the firs picture

--- src/front/js/component/CarouselTienda.js
+++ src/front/js/component/CarouselTienda.js
@@ -12,7 +12,7 @@ export const CarouselTienda = () => {
       {/* este es la carta */}
       <div className="card-group">
         <div className="card card-carousel">
-          <img className="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap" />
+          <img className="card-img-top" src="https://img1.wsimg.com/isteam/ip/dec62967-41b1-4326-9140-9f7297fee5d8/ols/PhotoRoom-20210820_184515.png/:/rs=w:1200,h:1200/:/rs=w:480,cg:true,m" alt="Card image cap" />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
