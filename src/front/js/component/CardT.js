e625bc7fc33ffd4b07050ea3440d9228def34191
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-24 23:29:24 +0000

    creating the cadT model

--- src/front/js/component/CardT.js
+++ src/front/js/component/CardT.js
@@ -1,3 +1,8 @@
 import React from "react";
 
-import CardT from "../../styles/CardT.css"
+import CardT from "../../styles/CardT.css";
+
+export const CardT = () => {
+  return;
+  <h1>Esto es una carta</h1>;
+};
