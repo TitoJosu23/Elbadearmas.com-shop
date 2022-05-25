dbb2d246a66818bedd0d65fe1ce3e0e79d23e048
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-25 22:51:01 +0000

    some changes in the CardT

--- src/front/js/component/CardT.js
+++ src/front/js/component/CardT.js
@@ -3,7 +3,19 @@ import React from "react";
 import CardT from "../../styles/CardT.css";
 
 export const CardT = () => {
-  return;
-  <h1>Esto es una carta</h1>;
-  <p>Hello world!</p>;
+  return (
+    <div className="card-carousel">
+      <img
+        className="card-img-top"
+        src="https://img1.wsimg.com/isteam/ip/dec62967-41b1-4326-9140-9f7297fee5d8/ols/PhotoRoom-20210820_184515.png/:/rs=w:1200,h:1200/:/rs=w:480,cg:true,m"
+        alt="Card image cap"
+      />
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">
+          <small className="text-muted">Last updated 3 mins ago</small>
+        </p>
+      </div>
+    </div>
+  );
 };
--- src/front/js/component/CarouselTienda.js
+++ src/front/js/component/CarouselTienda.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { CardT } from "./CardT.js";
 
 import "../../styles/CarouselTienda.css";
 
@@ -10,27 +11,25 @@ export const CarouselTienda = () => {
         <div></div>
       </div>
       {/* este es la carta */}
-      <div className="card-group">
+      <CardT />
+    </div>
+  );
+};
+
+{
+  /* <div className="carousel-row">
+      <div>
+        <h3 className="text-left">Tienda</h3>
+        <div></div>
+      </div>
+
+      <div className="card-group card-w">
         <div className="card card-carousel">
-          <img className="card-img-top" src="https://img1.wsimg.com/isteam/ip/dec62967-41b1-4326-9140-9f7297fee5d8/ols/PhotoRoom-20210820_184515.png/:/rs=w:1200,h:1200/:/rs=w:480,cg:true,m" alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-        <div className="card card-carousel">
-          <img className="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap" />
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
-        </div>
-        <div className="card card-carousel">
-          <img className="card-img-top" src="https://via.placeholder.com/150" alt="Card image cap" />
+          <img
+            className="card-img-top"
+            src="https://img1.wsimg.com/isteam/ip/dec62967-41b1-4326-9140-9f7297fee5d8/ols/PhotoRoom-20210820_184515.png/:/rs=w:1200,h:1200/:/rs=w:480,cg:true,m"
+            alt="Card image cap"
+          />
           <div className="card-body">
             <h5 className="card-title">Card title</h5>
             <p className="card-text">
@@ -39,6 +38,5 @@ export const CarouselTienda = () => {
           </div>
         </div>
       </div>
-    </div>
-  );
-};
+    </div> */
+}
