75451a569f1ebc6df91fbc66bcf4fe9a422701a4
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-28 07:18:11 +0000

    podcast rrearranged

--- src/front/js/pages/Podcast.js
+++ src/front/js/pages/Podcast.js
@@ -8,13 +8,17 @@ export const Podcast = () => {
   return (
     <div className="podcast-container">
       <h3>La Empresa</h3>
-      <div>
-        <img src="https://img1.wsimg.com/isteam/stock/DxeZmJo/:/rs=w:600,h:300,cg:true,m"></img>
-        <h3>Nuestro equipo</h3>
-        <p>
-          Cuéntales a las personas acerca de tus profesionales informáticos de
-          alto nivel.
-        </p>
+      <div className="center-div-podcast">
+        <div>
+          <img src="https://img1.wsimg.com/isteam/stock/DxeZmJo/:/rs=w:600,h:300,cg:true,m"></img>
+        </div>
+        <div>
+          <h3>Nuestro equipo</h3>
+          <p>
+            Cuéntales a las personas acerca de tus profesionales informáticos de
+            alto nivel.
+          </p>
+        </div>
       </div>
     </div>
   );
