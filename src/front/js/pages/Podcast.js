51e41df60be111cb83d846a8e4e7daecf6f9ed35
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-28 07:16:02 +0000

    podcast text added

--- src/front/js/pages/Podcast.js
+++ src/front/js/pages/Podcast.js
@@ -8,7 +8,14 @@ export const Podcast = () => {
   return (
     <div className="podcast-container">
       <h3>La Empresa</h3>
-      <img src="https://img1.wsimg.com/isteam/stock/DxeZmJo/:/rs=w:600,h:300,cg:true,m"></img>
+      <div>
+        <img src="https://img1.wsimg.com/isteam/stock/DxeZmJo/:/rs=w:600,h:300,cg:true,m"></img>
+        <h3>Nuestro equipo</h3>
+        <p>
+          Cuéntales a las personas acerca de tus profesionales informáticos de
+          alto nivel.
+        </p>
+      </div>
     </div>
   );
 };
