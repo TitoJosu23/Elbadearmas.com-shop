b9fd67fffc9dcb86b6b7b9e5ef562cda23fb3b2f
TitoJosu23 <josuearmas00z@gmail.com>
2022-05-28 07:19:58 +0000

    podcast classname everything

--- src/front/js/pages/Podcast.js
+++ src/front/js/pages/Podcast.js
@@ -9,10 +9,10 @@ export const Podcast = () => {
     <div className="podcast-container">
       <h3>La Empresa</h3>
       <div className="center-div-podcast">
-        <div>
+        <div className="podcast-img-1">
           <img src="https://img1.wsimg.com/isteam/stock/DxeZmJo/:/rs=w:600,h:300,cg:true,m"></img>
         </div>
-        <div>
+        <div className="text-1">
           <h3>Nuestro equipo</h3>
           <p>
             Cuéntales a las personas acerca de tus profesionales informáticos de
